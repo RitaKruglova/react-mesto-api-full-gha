9dd9ee06b5c11a7d09ec8d400231129f99c2affd
Rita Kruglova <kruglova@404@mail.ru>
2023-07-24 16:23:43 +0300

    fix: change content-security-policy

--- backend/helpers/handlingErrors.js
+++ backend/helpers/handlingErrors.js
@@ -3,7 +3,7 @@ module.exports.handleThen = (data, res, status = 200) => {
     res.status(404).send({ messege: 'Объект не найден' });
   } else {
     res.set({
-      'Content-Security-Policy': 'default-src "self"',
+      'Content-Security-Policy': 'default-src "self" mesto.rita-kruglova.nomoredomains.xyz',
     }).status(status).send({ data });
   }
 };
