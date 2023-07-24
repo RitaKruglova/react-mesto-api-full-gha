bfd0ad51a0d191924baebb5a1246f31114f36926
Rita Kruglova <kruglova@404@mail.ru>
2023-07-24 15:31:34 +0300

    fix: fix expressWinston

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
-const requestLogger = expressWinston({
+const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
   ],
